0b0d034362b2a626de3fce0198b9315f6526f6d4
Kgabo <kgabomokoka30@gmail.com>
2026-01-04 16:19:48 +0200

    General 2 span grid complete - need to add patern to header

--- app/components/trouphy.tsx
+++ app/components/trouphy.tsx
@@ -1,10 +1,28 @@
+import FeturedBadge from "./twoSpanTrophy";
+
 function Trouphy() {
   return (
     <>
-      <div className="container grid grid-rows-2 grid-cols-3">
-        <div className="col-span-2 bg-red-500">Block 1</div>
-        <div className="bg-blue-500">Block 2</div>
-
+      <div className="aboutHeading flex flex-col justify-center items-center">
+        <p className="text-light-green font-archivo-black text-sm py-2">
+          MATCH HIGHLIGHTS
+        </p>
+        <h1 className="font-bebas text-7xl">
+          <span className="text-text">TROPHY </span>
+          <span className="text-gold">CABINET</span>
+        </h1>
+        <p className="text-faded font-archivo py-2 mb-10">
+          A collection of my greatest hits - projects that showcase creativity,
+          technical skills, and champions mentality
+        </p>
+      </div>
+      <div className="container flex grid grid-rows-2 grid-cols-3">
+        {/* Row One */}
+        <div className="col-span-2 bg-dark-green rounded-2xl ml-16">
+          <FeturedBadge />
+        </div>
+        <div className="bg-dark-green rounded-2xl ml-16">Block 2</div>
+        {/* Row two */}
         <div className="bg-green-500">Block 3</div>
         <div className="bg-orange-500">Block 4</div>
         <div className="bg-purple-500">Block 5</div>
